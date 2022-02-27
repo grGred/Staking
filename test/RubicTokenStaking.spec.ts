377a77ff1dba3cbcab9d119285c37e554b2d44e1
Vladislav <81467635+grGred@users.noreply.github.com>
2022-02-27 20:44:57 +0300

    added whitelist

--- test/RubicTokenStaking.spec.ts
+++ test/RubicTokenStaking.spec.ts
@@ -198,6 +198,92 @@ describe('RubicTokenStaking', function () {
         });
     });
 
+    describe('Whitelist stake', () => {
+        it('Add to whitelist', async function () {
+            await RubicStaking.setWhitelist([Alice.address, Bob.address]);
+            await BRBC.connect(Carol).approve(RubicStaking.address, Web3.utils.toWei('30000', 'ether'));
+
+            expect(RubicStaking.connect(Carol).enterWhitelist(Web3.utils.toWei('10000', 'ether'))
+            ).to.be.rejectedWith("you are not in whitelist");
+
+            await BRBC.connect(Alice).approve(RubicStaking.address, Web3.utils.toWei('100000', 'ether'));
+            await RubicStaking.connect(Alice).enterWhitelist(Web3.utils.toWei('15000', 'ether'));
+            expect(RubicStaking.connect(Alice).enterWhitelist(Web3.utils.toWei('10001', 'ether'))
+            ).to.be.rejectedWith("more than limit per user");
+            await RubicStaking.connect(Alice).enter(Web3.utils.toWei('50000', 'ether')); // add 50 k in stake
+            await RubicStaking.connect(Alice).enterWhitelist(Web3.utils.toWei('10000', 'ether')); // add 10k in whitelist
+            let balanceAlice = await RubicStaking.balanceOf(Alice.address);
+            expect(balanceAlice.toString()).to.be.eq(Web3.utils.toWei('75000', 'ether'));
+        });
+
+        it('whitelist time ended', async function () {
+            await RubicStaking.setWhitelist([Alice.address, Bob.address]);
+            await BRBC.connect(Alice).approve(RubicStaking.address, Web3.utils.toWei('100000', 'ether'));
+            await BRBC.connect(Bob).approve(RubicStaking.address, Web3.utils.toWei('100000', 'ether'));
+
+            await RubicStaking.connect(Alice).enterWhitelist(Web3.utils.toWei('15000', 'ether'));
+
+            await network.provider.send('evm_increaseTime', [
+                Number(86400 + 100)
+            ]);
+            await network.provider.send('evm_mine');
+            /*
+            expect(await RubicStaking.connect(Alice).enterWhitelist(Web3.utils.toWei('10000', 'ether'))
+            ).to.be.rejectedWith("whitelist ended");
+            */
+            await RubicStaking.connect(Alice).enter(Web3.utils.toWei('50000', 'ether'));
+
+            let balanceAlice = await RubicStaking.balanceOf(Alice.address);
+            expect(balanceAlice.toString()).to.be.eq(Web3.utils.toWei('65000', 'ether'));
+        });
+
+        it('After whitelist', async function () {
+            for (let i = 0; i < 63; i++) {
+                await BRBC.mint(signers[i].address, '100000' + '0'.repeat(18));
+                await BRBC.connect(signers[i]).approve(
+                    RubicStaking.address,
+                    '10000000000000000' + '0'.repeat(18)
+                );
+                await RubicStaking.connect(signers[i]).enter('100000' + '0'.repeat(18));
+            }
+            for (let i = 64; i < 70; i++) {
+                await RubicStaking.setWhitelist([signers[i].address]);
+                await BRBC.mint(signers[i].address, '100000' + '0'.repeat(18));
+                await BRBC.connect(signers[i]).approve(RubicStaking.address, Web3.utils.toWei('30000', 'ether'));
+                await RubicStaking.connect(signers[i]).enterWhitelist(Web3.utils.toWei('25000', 'ether'));
+            } // now pool is 6.450.000, 550k are vacant
+            // expect(RubicStaking.endWhitelist()).to.be.rejectedWith("whitelist not ended");
+            await network.provider.send('evm_increaseTime', [
+                Number(86400 + 100)
+            ]);
+            await network.provider.send('evm_mine');
+
+            await RubicStaking.endWhitelist();
+            await network.provider.send('evm_mine');
+            let unfilled = await RubicStaking.unfilledAmount();
+            let max = await RubicStaking.maxRBCTotal();
+            expect(unfilled.toString()).to.be.eq(Web3.utils.toWei('550000', 'ether'));
+            expect(max.toString()).to.be.eq(Web3.utils.toWei('7000000', 'ether'));
+
+            for (let i = 70; i < 75; i++) {
+                await BRBC.mint(signers[i].address, '100000' + '0'.repeat(18));
+                await BRBC.connect(signers[i]).approve(
+                    RubicStaking.address,
+                    '10000000000000000' + '0'.repeat(18)
+                );
+                await RubicStaking.connect(signers[i]).enter('100000' + '0'.repeat(18));
+            }
+            await BRBC.mint(signers[76].address, '100000' + '0'.repeat(18));
+                await BRBC.connect(signers[76]).approve(
+                    RubicStaking.address,
+                    '10000000000000000' + '0'.repeat(18)
+                );
+            expect(RubicStaking.connect(signers[76]).enter('50001' + '0'.repeat(18))
+            ).to.be.rejectedWith("more than total limit");
+            await RubicStaking.connect(signers[76]).enter('50000' + '0'.repeat(18));
+        });
+    });
+
     describe('utility', () => {
         it('should not allow enter if not enough approve', async function () {
             await expect(
@@ -217,6 +303,7 @@ describe('RubicTokenStaking', function () {
                 Web3.utils.toWei('10000', 'ether')
             );
         });
+
         it('should display available tokens', async () => {
             await BRBC.approve(RubicStaking.address, Web3.utils.toWei('10000', 'ether'));
             await RubicStaking.enter(Web3.utils.toWei('10000', 'ether'));
@@ -240,17 +327,20 @@ describe('RubicTokenStaking', function () {
                 (await RubicStaking.canReceive(Web3.utils.toWei('10000', 'ether'))).toString()
             ).to.be.eq(Web3.utils.toWei('10000', 'ether'));
         });
+
         it('cant stake before start time', async () => {
             await RubicStaking.setStartDate('1738369056');
             expect(RubicStaking.enter('100')).to.be.rejectedWith('hasnt started yet');
         });
+
         it('cant stake more than user limit', async () => {
             expect(RubicStaking.enter('100001' + '0'.repeat(18))).to.be.rejectedWith(
                 'more than limit per user'
             );
         });
+
         it('cant stake more than total limit', async () => {
-            for (let i = 0; i < 70; i++) {
+            for (let i = 0; i < 63; i++) {
                 await BRBC.mint(signers[i].address, '100000' + '0'.repeat(18));
                 await BRBC.connect(signers[i]).approve(
                     RubicStaking.address,
@@ -262,8 +352,9 @@ describe('RubicTokenStaking', function () {
                 RubicStaking.connect(signers[71]).enter('100000' + '0'.repeat(18))
             ).to.be.rejectedWith('more than total limit');
         });
+
         it('can decrease total limit and increase again', async () => {
-            for (let i = 0; i < 70; i++) {
+            for (let i = 0; i < 63; i++) {
                 await BRBC.mint(signers[i].address, '100000' + '0'.repeat(18));
                 await BRBC.connect(signers[i]).approve(
                     RubicStaking.address,
@@ -292,6 +383,7 @@ describe('RubicTokenStaking', function () {
 
             RubicStaking.connect(signers[71]).enter('100000' + '0'.repeat(18)); // back to Limit (7100k)
         });
+
         it('sweep tokens test', async () => {
             const TestTokenFactory = await ethers.getContractFactory('TestERC20');
             const TestToken = await TestTokenFactory.deploy('100');
