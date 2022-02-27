593c7690fc818669c19b7736d75efdd2971698f6
Vladislav <81467635+grGred@users.noreply.github.com>
2022-02-27 17:29:25 +0300

    add view func

--- contracts/RubicTokenStaking.sol
+++ contracts/RubicTokenStaking.sol
@@ -15,7 +15,7 @@ contract RubicTokenStaking is FreezableToken, Ownable {
     uint256 constant MAX_BRBC_PER_WHITELIST = 25000 ether;
     uint256 constant MIN_BRCB = 1000 ether;
 
-    uint256 public MAX_BRBC_TOTAL = 7000000 ether;
+    uint256 public maxRBCTotal = 7000000 ether;
     uint256 public whitelistPool = 700000 ether;
     uint256 public freezeTime = 86400;
     uint256 public totalRBCEntered;
@@ -57,7 +57,7 @@ contract RubicTokenStaking is FreezableToken, Ownable {
         uint256 newTotal = totalRBCEntered.add(_amount);
 
         require(newEntered <= MAX_BRBC_PER_USER, "more than limit per user");
-        require(newTotal <= MAX_BRBC_TOTAL, "more than total limit");
+        require(newTotal <= maxRBCTotal, "more than total limit");
         // Gets the amount of BRBC locked in the contract
         uint256 totalBRBC = BRBC.balanceOf(address(this));
         // Gets the amount of xBRBC in existence
@@ -116,33 +116,13 @@ contract RubicTokenStaking is FreezableToken, Ownable {
             .div(totalShares);
         _burn(msg.sender, xRBCAmount);
         BRBC.transfer(msg.sender, BRBCToReceive);
-
-        // lastRewardAmount is RBC amount we added as a reward
-        // So out MAX_BRBC_TOTAL will be always around 7m, but this number won't be accurate
-        // maybe it can be more accurate if lastRewardAmount will be decrease partially
-        // Here we don't need to take off transfer func of xRBC
-        // This variant can cause problems with frontend part (will need to make one more view func?)
+        // Calculates new maxRBCTotal
         if (lastRewardAmount > 0) {
-            MAX_BRBC_TOTAL = MAX_BRBC_TOTAL.add(BRBCToReceive).sub(lastRewardAmount);
+            maxRBCTotal = maxRBCTotal.add(BRBCToReceive).sub(lastRewardAmount);
             lastRewardAmount = 0;
         } else {
-            MAX_BRBC_TOTAL = MAX_BRBC_TOTAL.add(BRBCToReceive);
+            maxRBCTotal = maxRBCTotal.add(BRBCToReceive);
         }
-
-        /*
-        // One more variant of realisation
-        // MAX_BRBC_TOTAL is always 7mil
-        // user can reenter after he leaves staking from it's address
-        // no need to make addReward func
-        // if user transfer his xRBC to other address, max pool won't become smaller, so we need to forbidden transfers
-        if (userEnteredAmount[msg.sender] >= BRBCToReceive) {
-            MAX_BRBC_TOTAL = MAX_BRBC_TOTAL.add(userEnteredAmount[msg.sender]);
-            userEnteredAmount[msg.sender] = 0;
-        } else {
-            MAX_BRBC_TOTAL = MAX_BRBC_TOTAL.add(BRBCToReceive);
-            userEnteredAmount[msg.sender] = userEnteredAmount[msg.sender].sub(BRBCToReceive);
-        }
-        */
         emit Left(msg.sender, xRBCAmount, BRBCToReceive);
     }
 
@@ -153,6 +133,10 @@ contract RubicTokenStaking is FreezableToken, Ownable {
             "amount is greater than total xBRCB amount"
         );
         return _amount.mul(BRBC.balanceOf(address(this))).div(totalShares);
+    }
+
+    function unfilledAmount() external view returns (uint256) {
+        return maxRBCTotal.sub(totalRBCEntered);
     }
 
     function setFreezeTime(uint256 _freezeTime) external onlyOwner {
@@ -171,8 +155,12 @@ contract RubicTokenStaking is FreezableToken, Ownable {
 
     function endWhitelist() external onlyOwner {
         require(block.timestamp > startDate + 1 days, "whitelist not ended");
-        MAX_BRBC_TOTAL = MAX_BRBC_TOTAL.add(whitelistPool);
+        maxRBCTotal = maxRBCTotal.add(whitelistPool);
         whitelistPool = 0;
+    }
+
+    function changeTotal(uint256 _newTotal) external onlyOwner {
+        maxRBCTotal = _newTotal;
     }
 
     function addRewards(uint256 _amount) external onlyOwner {
